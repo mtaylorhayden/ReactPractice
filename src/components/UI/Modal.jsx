2d473c5ae1290a5a45aae014bba849e14a4df3af
Max Taylor-Hayden <nebraskamax@gmail.com>
2022-10-23 11:28:22 -0600

    init commit

---
+++ src/components/UI/Modal.jsx
@@ -0,0 +1,30 @@
+import { createPortal } from "react-dom";
+import classes from "./Modal.module.css";
+
+const BackDrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onClick}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const portalElement = document.getElementById("overlays");
+
+const Modal = (props) => {
+  return (
+    <>
+      {createPortal(<BackDrop onClick={props.onClick} />, portalElement)}
+      {createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </>
+  );
+};
+
+export default Modal;
